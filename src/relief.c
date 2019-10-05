cb5058df709ad8d66144b42480bc559a19c03a84
xChygyNx <Chykha110@gmail.com>
2019-10-05 15:49:01 +0300

    relief

---
+++ src/relief.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   relief.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcredibl <pcredibl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/05 14:47:35 by pcredibl          #+#    #+#             */
+/*   Updated: 2019/10/05 14:59:10 by pcredibl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+static int	average_height(t_fdf *fdf)
+{
+	
+}
+
+void	change_relief(t_fdf *fdf, int dif)
+{
+	int		ah;
+
+	ah = average_height(fdf);
+
+}
